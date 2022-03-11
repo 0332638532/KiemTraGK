593017ea0acad1701eefcb7c0ae2dd16f68b319f
Pham Van Khanh <khanhphamvan1910@gmail.com>
2022-03-11 14:51:17 +0700

    commit

--- loginform/Form1.Designer.cs
+++ loginform/Form1.Designer.cs
@@ -30,32 +30,57 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.USERNAME = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(186, 122);
+            this.label1.Location = new System.Drawing.Point(248, 150);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.Size = new System.Drawing.Size(71, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "username";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // USERNAME
             // 
-            this.USERNAME.Location = new System.Drawing.Point(295, 115);
+            this.USERNAME.Location = new System.Drawing.Point(393, 142);
+            this.USERNAME.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.USERNAME.Name = "USERNAME";
-            this.USERNAME.Size = new System.Drawing.Size(194, 20);
+            this.USERNAME.Size = new System.Drawing.Size(257, 22);
             this.USERNAME.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Matkhau";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Location = new System.Drawing.Point(393, 220);
+            this.txtmatkhau.Margin = new System.Windows.Forms.Padding(4);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(257, 22);
+            this.txtmatkhau.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.USERNAME);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -67,6 +92,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox USERNAME;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtmatkhau;
     }
 }
 
